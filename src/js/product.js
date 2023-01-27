18ad65784c6759d6587c768a3cabfb83d853f225
dcarsin <dcarsin@byui.edu>
2023-01-26 23:18:06 -0300

    on week4

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
-import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
--- src/tests/jest/Main.test.ts
+++
@@ -1,7 +0,0 @@
-import "jest";
-
-describe("Coverage test insurance for Main.ts", () => {
-  it("makes truthy assertion", () => {
-    expect(true).toBeTruthy();
-  });
-});
