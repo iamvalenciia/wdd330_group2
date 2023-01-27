8e8d046f782b24c512a9e5c22be800aef86c86d7
EdgarJRM <edgarrodriguezm@hotmail.com>
2023-01-26 21:43:56 -0500

    cambio

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,12 @@
+import { getParam } from "./utils.mjs";
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
